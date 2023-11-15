df293fc832f34e1627204c48abc7e0212bcf0f85
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-11-15 16:15:35 +0200

    Temporary Logistic is done

--- Assign1 [markdown].py
+++ Assign1 [markdown].py
@@ -1,3 +1,10 @@
+# %% [markdown]
+# # Assignment-1 Abdelrahman Sayed 
+
+# %% [markdown]
+# ## Import Libraries
+
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,23 +17,107 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
 
-dataset = pd.read_csv('C:/Users/User/Desktop/ML_dataset/loan_old.csv')
+# %% [markdown]
+# ## Import Data From Dataset
+
+# %%
+dataset = pd.read_csv('loan_old.csv')
+
+
+# %% [markdown]
+# ## Analyze some data
+
+# %% [markdown]
+# ### Checking Missing Values
+# 
+
+# %%
 missing_values = dataset.isnull().sum()
 print("Missing Values:\n", missing_values)
+
+# %% [markdown]
+# ### Type of Features
+
+# %%
 feature_types = dataset.dtypes
 print("\nFeature Types:\n", feature_types)
+
+# %% [markdown]
+# ### Check The Scale
+
+# %%
 numerical_features = dataset.select_dtypes(include=['int64', 'float64'])
 feature_scales_std = numerical_features.std()
 print("\nFeature Scales (Standard Deviation):\n", feature_scales_std)
+
+# %% [markdown]
+# ### Visualize pairplot between numerical values
+# 
+
+# %%
+sns.pairplot(numerical_features)
+plt.show()
+
+# %% [markdown]
+# # Assignment-1 Seif El Din Mohamed
+
+# %% [markdown]
+# ### Dataset Size before removing records
+
+# %%
 dataset.shape
+
+# %% [markdown]
+# ### Drop Records with empty values
+
+# %%
 dataset.dropna(inplace=True)
 dataset
+
+# %% [markdown]
+# ### Check Missing Values after Deleting empty value records
+# 
+
+# %%
 missing_values = dataset.isnull().sum()
 print("Missing Values:\n", missing_values)
+
+# %% [markdown]
+# ### Dataset Size after removing records
+
+# %%
+dataset.shape
+
+# %% [markdown]
+# ## Separate Dataset
+
+# %%
 X =  dataset.iloc[: , 1: -2].values #dataset.iloc[: , [0]].values  pandas series 
 Y1 = dataset.iloc[:, -2].values
 Y2 = dataset.iloc[:, -1].values
+
+
+# %% [markdown]
+# ## Train & Test Split
+
+# %%
 X_train, X_test, Y1_train, Y1_test, Y2_train, Y2_test = train_test_split(X, Y1, Y2, test_size=0.2, random_state=1)
+
+# %%
+print(X_train)
+
+# %%
+print(Y1_train)
+
+
+# %%
+print(Y2_train)
+
+# %% [markdown]
+# ## Encoding Features
+
+# %%
+
 le = LabelEncoder()
 
 X_train[:,0] = le.fit_transform(X_train[:,0]) # 1/0 -- Male/Female
@@ -45,8 +136,22 @@ X_test[:,3] = le.fit_transform(X_test[:,3]) # 1/0 Not Graduate/Graduate
 ct = ColumnTransformer( transformers = [('encoder', OneHotEncoder(), [8])], remainder='passthrough') 
 X_train = np.array(ct.fit_transform(X_train)) # fit_transform doesn't return Numpy array 
 X_test = np.array(ct.fit_transform(X_test)) # fit_transform doesn't return Numpy array 
-Y2_train = le.fit_transform(Y2_train) # 1/0 Y/N
+
+
+
+# %% [markdown]
+# ## Encoding Targets
+
+# %%
+Y2_train = le.fit_transform(Y2_train) # 1/0 Y/N 
 Y2_test = le.fit_transform(Y2_test) # 1/0 Y/N
+
+
+# %% [markdown]
+# ##  numerical features are standardized
+# 
+
+# %%
 nf1 = X_train[:, 7:10]  
 nf2 = X_test[:, 7:10]  
 
@@ -61,12 +166,40 @@ nf_standardized2 = scaler.fit_transform(nf2)
 X_train[:, 7:10]   = nf_standardized1
 X_test[:, 7:10]   = nf_standardized2
 
+# %%
+X_train[:,7:10]
 
-import numpy as np
+# %% [markdown]
+# ## Linear Regression
+# 
 
+# %% [markdown]
+# ### Train & Fit The Model
 
+# %%
+linear_model = LinearRegression()
+linear_model.fit(X_train, Y1_train)
 
+# %% [markdown]
+# ### Predict New Values
 
+# %%
+Y_predict  = linear_model.predict(X_test)
+
+# %% [markdown]
+# ### R-Squared Error Evaluation
+
+# %%
+r2 = r2_score(Y1_test, Y_predict)
+print(f'R-squared score: {r2}')
+
+# %%
+print(X_train[:,7:])
+
+# %% [markdown]
+# ## Logistic Regression From Scratch
+
+# %%
 def sigmoid(z):
     """Sigmoid activation function"""
     return 1 / (1 + np.exp(-z))
@@ -74,12 +207,12 @@ def sigmoid(z):
 def initialize_parameters(dim):
     """Initialize weights and bias to zeros"""
     w = np.zeros((dim, 1))
-    b = 0
+    b = np.zeros((X_train.shape[0], 1))  # Ensure the correct shape
     return w, b
 
 def forward_propagation(X, w, b):
     """Forward propagation to calculate the predicted values"""
-    z = np.array(np.dot(X, w),dtype=np.float32) + b
+    z = np.dot(X, w) + b
     A = sigmoid(z)
     return A
 
@@ -128,44 +261,12 @@ def logistic_regression(X_train, Y_train, num_iterations=1000, learning_rate=0.0
 
     return w, b
 
-# Training the logistic regression model
+# %% [markdown]
+# ## Train Logistic Regression Model
+
+# %%
 num_iterations = 1000
 learning_rate = 0.01
-w, b = logistic_regression(X_train, Y2_train, num_iterations, learning_rate)
+w, b = logistic_regression(np.array(X_train), np.array(Y2_train), num_iterations, learning_rate)
 
 
-# Now, you can test the model on the new data
-# Ensure that you preprocess the new data in the same way as the training data before testing.
-
-# Preprocess the new data
-new_data = pd.read_csv('C:/Users/User/Desktop/ML_dataset/loan_new.csv')  # Replace 'path_to_new_data.csv' with the actual path
-new_data.Gender = lbl.fit_transform(new_data.Gender)
-new_data.Married = lbl.fit_transform(new_data.Married)
-new_data.Dependents = lbl.fit_transform(new_data.Dependents)
-new_data.Education = lbl.fit_transform(new_data.Education)
-new_data.Property_Area = lbl.fit_transform(new_data.Property_Area)
-
-# Standardize numerical features
-new_data[numerical_feature_columns] = scaler.transform(new_data[numerical_feature_columns])
-
-# Extract features for testing
-X_new_test = new_data.iloc[:, 1:-2].values
-
-# Make predictions on the new data
-predictions = forward_propagation(X_new_test, w, b)
-
-# Convert predicted probabilities to binary predictions (0 or 1)
-predictions_binary = (predictions >= 0.5).astype(int)
-
-# Display the predictions
-print("Predictions on the new data:\n", predictions_binary)
-
-
-# Save predictions to a text file
-output_file_path = 'C:/Users/User/Desktop/ML_dataset/Predictions.txt'
-
-with open(output_file_path, 'w') as file:
-    for prediction in predictions_binary:
-        file.write(f'{prediction}\n')
-
-print(f'Predictions saved to {output_file_path}')
